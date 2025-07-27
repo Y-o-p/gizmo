89b84ff9facd4c197cfcf9c210c04e77c88c85a1
Jesse Rheal <jessecolerheal@live.com>
2025-07-26 21:57:07 -0500

    [#23] Added second window for the command graph.

--- gizmo/scripts/command_stack_container.gd
+++ gizmo/scripts/command_stack_container.gd
@@ -2,28 +2,12 @@ extends VBoxContainer
 
 func _ready() -> void:
 	User.command.command_completed.connect(_on_command_completed)
-	User.command.started_command.connect(_on_command_started)
 	User.command.commands_refreshed.connect(_on_commands_refreshed)
 
 func _on_command_completed(command: String):
 	var label = Label.new()
 	label.text = command
 	add_child(label)
-
-func _on_command_started(parameter_callback: Callable):
-	var edit = LineEdit.new()
-	add_child(edit)
-	edit.grab_focus()
-	edit.text_submitted.connect(func(parameters: String):
-		edit.queue_free()
-		parameter_callback.call(parameters)
-	)
-	edit.editing_toggled.connect(func(editing: bool):
-		if editing:
-			return
-		
-		edit.queue_free()
-	)
 
 func _on_commands_refreshed(stack: PackedStringArray):
 	for child in get_children():
--- gizmo/scripts/gui.gd
+++ gizmo/scripts/gui.gd
@@ -1,8 +1,9 @@
-extends CanvasLayer
+extends MarginContainer
 
 func _ready() -> void:
 	User.command.invalid_command.connect(_on_invalid_command)
-	
+	User.command.started_command.connect(_on_command_started)
+
 func _on_invalid_command(error: String):
 	var label = Label.new()
 	label.text = error
@@ -13,4 +14,19 @@ func _on_invalid_command(error: String):
 	tween.tween_property(label, "modulate", Color(label.modulate.r, label.modulate.g, label.modulate.b, 0), 1.0)
 	tween.finished.connect(func():
 		label.queue_free()
+	)
+
+func _on_command_started(parameter_callback: Callable):
+	var edit = LineEdit.new()
+	add_child(edit)
+	edit.grab_focus()
+	edit.text_submitted.connect(func(parameters: String):
+		edit.queue_free()
+		parameter_callback.call(parameters)
+	)
+	edit.editing_toggled.connect(func(editing: bool):
+		if editing:
+			return
+		
+		edit.queue_free()
 	)
