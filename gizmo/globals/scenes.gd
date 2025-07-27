d0a219950f95db5c7a3351b2d939109606c64a41
Jesse Rheal <jessecolerheal@live.com>
2025-07-26 21:32:15 -0500

    Removed unused scenes and scripts.

--- gizmo/globals/scenes.gd
+++ gizmo/globals/scenes.gd
@@ -1,7 +1,1 @@
 extends Node
-
-# 2d view scenes
-var surface = preload("res://scenes/surface.tscn")
-
-# GUI elements
-var vertex_control = preload("res://scenes/vertex_control.tscn")
--- gizmo/scripts/vertex_control.gd
+++
@@ -1,9 +0,0 @@
-extends Button
-class_name VertexControl
-
-var update_vertex: Callable
-
-func _process(delta: float) -> void:
-	if button_pressed:
-		position = get_global_mouse_position()
-		update_vertex.call(position)
